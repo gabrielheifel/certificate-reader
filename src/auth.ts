84423ef529d28d81a46e7ca1633701f04187fb34
Gabriel Heifel <gabrielheifel@gmail.com>
2024-05-22 20:59:11 -0300

    [auth] callbacks and update schema

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,21 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, UserRole } from "@prisma/client"
 
 import authConfig from "@/auth.config"
 import { db } from "@/lib/db"
+import { getUserById } from "@/services/database/UserService"
 // const prisma = new PrismaClient()
+
+type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -12,6 +23,31 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ token })
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig
